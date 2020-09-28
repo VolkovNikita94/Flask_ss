860b01a456e2b1fbf809a290a091efeb79b16a91
VolkovNikita94 <volkovnikita94@gmail.com>
2020-09-28 09:37:35 +0300

    _i_Page_cfg.js update - added no cache control

--- userdata/ucmc2020ssRoot/page/_i_c_Page_cfg.js
+++ userdata/ucmc2020ssRoot/page/_i_c_Page_cfg.js
@@ -25,7 +25,8 @@ function handleLoadedpage_cfg(page_cfgData) {
 
 function loadpage_cfg() {
 	var request = new XMLHttpRequest();
-	request.open("GET", "Json_models/Page_cfg.json", false);
+	request.open("GET", "Json_models/Page_cfg.json", true);
+	request.setRequestHeader('cache-control', 'no-cache');
 	
 	try {
 		request.send();
@@ -42,7 +43,8 @@ function loadpage_cfg() {
 
 function loadpage_about() {
 	var request = new XMLHttpRequest();
-	request.open("GET", "Json_models/Page_about.json", false);
+	request.open("GET", "Json_models/Page_about.json", true);
+        request.setRequestHeader('cache-control', 'no-cache');
 	
 	try {
 		request.send();
